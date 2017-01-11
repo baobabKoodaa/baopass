dc03b34433c1f34e4e8c058c4f8249c7eb18c294
baobabKoodaa <baobabKoodaa@users.noreply.github.com>
2017-01-12 01:28:01 +0200

    Reliably detect incorrect password with AES GCM Mode

--- src/crypto/AES.java
+++ src/crypto/AES.java
@@ -1,50 +1,51 @@
 package crypto;
 
 import javax.crypto.*;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.SecureRandom;
 
 public class AES {
 
-    static final String cipherInstance = "AES/CBC/PKCS5Padding";
+    static final String cipherInstance = "AES/GCM/NoPadding";
 
     /** Iterations for PBKDF2 when generating encryption key from master password.
      *  Needs to be high, because user chosen passwords may be of low quality. */
     static final int ITERATIONS = 10000;
     static final int KEY_LENGTH_BYTES = 16;
 
-    //TODO: Change to GCM mode to detect incorrect password reliably?
-
     public static EncryptedMessage encrypt(final byte[] dataToEncrypt, final char[] password) throws Exception {
         /* Generate random IV and random salt. */
         SecureRandom rng = new SecureRandom();
         byte[] salt = new byte[32]; /* Can be any size. */
-        byte[] iv = new byte[16]; /* Must be AES block size, 128 bits. */
+        byte[] ivBytes = new byte[16]; /* Must be AES block size, 128 bits. */
         rng.nextBytes(salt);
-        rng.nextBytes(iv);
+        rng.nextBytes(ivBytes);
 
         /* In order to encrypt we need to turn password into 256 random-looking bits. */
         SecretKeySpec AESkey = generateAESkey(password, salt);
 
         /* Encryption. */
         Cipher aes = Cipher.getInstance(cipherInstance);
-        aes.init(Cipher.ENCRYPT_MODE, AESkey, new IvParameterSpec(iv));
+        GCMParameterSpec iv = new GCMParameterSpec(16 * Byte.SIZE, ivBytes);
+        aes.init(Cipher.ENCRYPT_MODE, AESkey, iv);
         byte[] encryptedData = aes.doFinal(dataToEncrypt);
 
         /* In order to decrypt later, we need to save iv, salt, iterations and key length. */
-        return new EncryptedMessage(encryptedData, salt, iv, ITERATIONS, KEY_LENGTH_BYTES);
+        return new EncryptedMessage(encryptedData, salt, ivBytes, ITERATIONS, KEY_LENGTH_BYTES);
     }
 
     public static byte[] decrypt(final EncryptedMessage encryptedMessage, final char[] password) throws Exception {
         byte[] cipherText = encryptedMessage.getCipherText();
         byte[] salt = encryptedMessage.getSalt();
-        byte[] iv = encryptedMessage.getIv();
+        byte[] ivBytes = encryptedMessage.getIv();
         int iterations = encryptedMessage.getIterationsPBKDF2();
         int keyLength = encryptedMessage.getKeyLengthPBKDF2();
         SecretKeySpec AESkey = generateAESkey(password, salt);
         Cipher aes = Cipher.getInstance(cipherInstance);
-        aes.init(Cipher.DECRYPT_MODE, AESkey, new IvParameterSpec(iv));
+        GCMParameterSpec iv = new GCMParameterSpec(16 * Byte.SIZE, ivBytes);
+        aes.init(Cipher.DECRYPT_MODE, AESkey, iv);
         byte[] decryptedData = aes.doFinal(cipherText);
         return decryptedData;
     }
